0070b1fc3477c9f884cd280f0f30cc45c8784312
Divakar Patil <divakarpatil35@gmail.com>
2024-12-01 19:18:14 +1100

    2024 - day 1

---
+++ 2024/q01.py
@@ -0,0 +1,40 @@
+import os
+from collections import Counter
+import heapq
+
+
+def part_1(data: str) -> int:
+    left_list: list[int] = []
+    right_list: list[int] = []
+    for line in data.split("\n"):
+        left, right = line.split("  ")
+        heapq.heappush(left_list, int(left))
+        heapq.heappush(right_list, int(right))
+
+    count = 0
+    for _ in range(len(left_list)):
+        count += abs(heapq.heappop(left_list) - heapq.heappop(right_list))
+    return count
+
+
+def part_2(data: str) -> int:
+    left_list: list[int] = []
+    right_list: list[int] = []
+    for line in data.split("\n"):
+        left, right = line.split("  ")
+        left_list.append(int(left))
+        right_list.append(int(right))
+
+    counter = Counter(right_list)
+    count = 0
+    for ele in left_list:
+        count += counter[ele] * ele
+
+    return count
+
+
+with open(os.path.join(os.getcwd(), "2024/inputs/1.txt"), "r") as f:
+    data = f.read().strip()
+
+    print(f"Part one output: {part_1(data)}")
+    print(f"Part two output: {part_2(data)}")
